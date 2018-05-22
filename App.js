865ab5cc393e409823fc36f3b43c2c90b14ab95c
John Higgins <jhiggins0@gmail.com>
2018-05-22 13:08:21 -0700

    successfully ejected

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, ScrollView } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
